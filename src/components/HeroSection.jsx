83fe763b6e0b0ba7e36e742edcfd34e92005d86a
Tuan Pham <tuanqpham0921@gmail.com>
2025-08-07 17:01:09 -0500

    updated resume

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -49,7 +49,7 @@ export const HeroSection = () => {
                                 GitHub
                             </a>
                             <a
-                                href="https://drive.google.com/file/d/1MHKzgp-9FdsJNb86ketAg3ry5CjN0dxy/view?usp=sharing"
+                                href="https://drive.google.com/file/d/1hU6hBgARFgoDqQtddUAF7I0UdbgtqGdD/view?usp=sharing"
                                 className="text-sm text-muted-foreground hover:underline"
                                 target="_blank"
                             >
